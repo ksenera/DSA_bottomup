873f1a04bbe013a282ae4be55eed32aa416bb0b4
ksenera <ksenera7130@conestogac.on.ca>
2026-03-07 22:44:58 -0500

    print for visualization

--- reverse_list.py
+++ reverse_list.py
@@ -3,9 +3,28 @@ def reverse_list(head):
     curr = head 
 
     while curr is not None:
+        print(f"prev={prev.val if prev else None}, curr={curr.val}, next={curr.next.val if curr.next else None}")
         next_node = curr.next # next is a property of the current node we are standing on
         curr.next = prev
-        prev = 
-        curr = 
+        prev = curr 
+        curr = next_node 
+        print(f" after flip: prev={prev.val}, curr={curr.val if curr else None}")
 
     return prev 
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val 
+        self.next = None 
+
+a = Node("A")
+b = Node("B")
+c = Node("c")
+d = Node("D")
+
+a.next = b 
+b.next = c 
+c.next = d 
+
+result = reverse_list(a)
