7ebcbacf638a25286d30bd15c940c9800f623c38
ksenera <ksenera7130@conestogac.on.ca>
2026-03-07 22:30:55 -0500

    next_node is the variable i am saving: if curr is the node and each node has a .next property pointint to next node => next_node = curr.next

---
+++ reverse_list.py
@@ -0,0 +1,11 @@
+def reverse_list(head):
+    prev = None 
+    curr = head 
+
+    while curr is not None:
+        next_node = curr.next # next is a property of the current node we are standing on
+        curr.next = 
+        prev = 
+        curr = 
+
+    return prev 
