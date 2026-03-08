96694e2b5d214952ac6808b22e50cee5816bf9d9
ksenera <ksenera7130@conestogac.on.ca>
2026-03-08 00:39:38 -0500

    3 errors

--- active_recall.py
+++ active_recall.py
@@ -4,5 +4,17 @@ def linked_list(head):
     # define head no put the parameter into a variable 
     curr = head # save into curr 
     prev = None # None -> A -> B -> C -> D -> None 
-    # wanna get D -> C -> B -> A - > None 
-    
+    # wanna get D -> C -> B -> A -> None 
+
+    # while A is not None == True 
+    while curr is not None:
+        # i have two errors in the while loop 
+        # next node to A is B = A.next == B
+        next_node = curr.next # get next node
+        # B = prev overrides None 
+        curr.next = prev # reverse it 
+        prev = next 
+        next = None
+    # 1 error here 
+    print(next)
+
