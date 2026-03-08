72023b23da5af8699ed4376743da7fbf6daa7254
ksenera <ksenera7130@conestogac.on.ca>
2026-03-08 00:27:24 -0500

    created and defined what a node is has a val, has left and right child each instance initialized to None

--- tree.py
+++ tree.py
@@ -2,3 +2,16 @@ def find_max(node):
     if node is None:
         return float('-inf')
     # did 0 first but what if a tree contains only negative numbers. 
+    left_max = find_max()
+    # left_max = find_max(node.val) WRONG 
+    # need to pass a node, find_max expects a node object not a number
+    # find_max function needs to the whole node to access both 
+    # .val and .next children 
+    # if im standing at curr_node -> child1Left && curr_node -> child2Right how do i 
+    # access left child of curr_node
+
+class Node:
+    def __init__(self, val):
+        self.val = val 
+        self.left = None 
+        self.right = None 
