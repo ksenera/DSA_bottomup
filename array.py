1b03acde3443ede00464eef44277289ed3515f1a
ksenera <ksenera7130@conestogac.on.ca>
2026-03-08 20:34:21 -0400

    comment

--- array.py
+++ array.py
@@ -7,8 +7,22 @@ for i in list:
     curr_sum += i
     if curr_sum <= 0:
         curr_sum = 0
+    # Also the best_sum = curr_sum should not be 
+    # inside the else. It should be independent. 
+    # What if curr_sum is positive but you just 
+    # reset it? you'd miss updating best_sum
+    
     else:
         best_sum = curr_sum
         
         print(best_sum) 
 
+# cleaner version 
+for i in list:
+    curr_sum += i
+    if curr_sum < 0:
+        curr_sum = 0
+    if curr_sum > best_sum:
+        best_sum = curr_sum
+
+print(best_sum)
