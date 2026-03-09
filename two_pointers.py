101798949c0de9165dea68ad60bc3cdb03ae9068
ksenera <ksenera7130@conestogac.on.ca>
2026-03-09 00:41:44 -0400

    two pointers | error: multiple

--- two_pointers.py
+++ two_pointers.py
@@ -17,13 +17,53 @@ ___________PLAIN ENGLISH___________
    - no solution given all params
 
 REMEMBER: two pointers = one ptr at start and one ptr at end
-          *-----> <-----* move them toward each other. 
+ *-----> <-----* move them toward each other. 
+ 3) when do i move left ptr right and right ptr left?
+   - since the list is sorted from smallest num to largest
+    - move left ptr first WRONG 
+   - to make sum smaller do i want bigger number or smaller number?
+    - smaller number 
+     - right ptr moves left to hit every num to get to smaller vals
+   - if sum is too small: 
+    - move left ptr right to get bigger value 
+   - sum > target -> move R ptr L
+   - sum < target -> move L ptr R 
+   - sum == target -> found, return indices
 
 """
 
-#_________FUNCTION SKELETON_________
-def find_two(nums, target):
-    pass
+#___________FUNCTION___________
+def find_two(nums, target):    
+    # take care of empty list edge case
+    if not nums:
+        return []
+    
+    # define left and right ptrs 
+    left = 0 
+    right = len(nums) - 1
+    
+    # WRONG
+    # sum = nums[left] + nums[right]
+    # if sum > target:
+    #     nums[right - 1] 
+    # if sum < target:
+    #     nums[left + 1]
+    # if sum == target:
+    #     return sum
+
+
+    while left < right:
+        # check sum 
+        sum = nums[left] + nums[right]
+        if sum > target:
+            right -= 1
+        if sum < target:
+            left += 1
+        if sum == target:
+            # WRONG
+            #return nums[left, right]
+            return [left, right]
+
 
 #_________ASSERT STATEMENTS_________
 # NORMAL CASE: nums = [2, 7, 11, 15], target = 9 
@@ -31,7 +71,7 @@ def find_two(nums, target):
 assert find_two(nums, target) == [0, 1]
 
 # EMPTY LIST: nums = [], target = 9
-assert find_two(nums, target) == [] 
+assert find_two([], target) == [] 
 #wrong - assert find_two(nums, target) == IndexError 
 
 # NO SOLN EXISTS: nums = [2, 7, 11, 15], target = 100
@@ -54,4 +94,15 @@ my assertion for empty is wrong:
 None for no solution works too but be consistent:
     either [] for none or None for none
 
+"""
+
+"""
+_______IMMEDIATE ERROR LOG_______
+Problem 1: no loop. checked sum once and stopped 
+ - need to keep checking till pointers meet 
+ - what loop runs untul a condition is false 
+Problem 2: not updating pointers:
+ - accessing value nums[right -1] but not saving it anywhere
+Problem 3: wrong return:
+ - return sum returns value i need to return indcies 
 """
