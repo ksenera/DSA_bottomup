e700d23776f14896106d2f8f615e028b9618cce9
ksenera <ksenera7130@conestogac.on.ca>
2026-03-08 23:33:01 -0400

    given problem statement, plain english logic work through questions for constraints and assert statements section

---
+++ two_pointers.py
@@ -0,0 +1,20 @@
+#______________GIVEN________________
+'''Given a sorted list of integers find two numbers that 
+   add up to the target. Return their indices. 
+   Ex: 2 + 7 = 9 =====> nums[0] + nums[1] = target 
+'''
+nums = [2, 7, 11, 15]
+target = 9 
+
+"""
+___________PLAIN ENGLISH___________
+ 1) what are my edge cases?
+   - no numbers add up to target 
+   - empty list 
+   - negative numbers only 
+
+ 2) what boundary conditions exist?
+   - 
+
+"""
+#_________ASSERT STATEMENTS_________
