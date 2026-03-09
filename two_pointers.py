02ca36b21ef6626a95c7e7381b6ace49faebf952
ksenera <ksenera7130@conestogac.on.ca>
2026-03-08 23:45:42 -0400

    empty list case

--- two_pointers.py
+++ two_pointers.py
@@ -23,9 +23,14 @@ def find_two(nums, target):
     pass
 
 #_________ASSERT STATEMENTS_________
-# NORMAL CASE nums = [2, 7, 11, 15], target = 9 
+# NORMAL CASE: nums = [2, 7, 11, 15], target = 9 
 # Ex: 2 + 7 = 9 =====> nums[0] + nums[1] = target
 assert find_two(nums, target) == [0, 1]
+
+# EMPTY LIST: nums = [], target = 9
+assert find_two(nums, target) == None 
+
+# NO SOLN EXISTS:
 
 """
 _______IMMEDIATE FEEDBACK LOG_______
