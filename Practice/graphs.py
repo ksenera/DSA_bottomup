4a2ee205004c4538fb8bfd896425facf36b3f286
ksenera <ksenera7130@conestogac.on.ca>
2026-03-28 22:37:29 -0400

    graphs: LC323 | scaffold comment

---
+++ Practice/graphs.py
@@ -0,0 +1,23 @@
+"""
+LC323 — Number of Connected Components in an Undirected Graph
+You have n nodes labeled 0 to n-1 and a list of undirected edges.
+Return the number of connected components.
+
+Input: n = 5, edges = [[0,1],[1,2],[3,4]]
+Output: 2
+
+Input: n = 5, edges = [[0,1],[1,2],[2,3],[3,4]]
+Output: 1
+
+PREDICTION:
+SIGNAL:
+FAMILY:
+CATEGORY:
+LOCAL:
+WHAT DO I TRACK:
+BASE CASE:
+COMBINE:
+INPUT:
+OUTPUT:
+TRANSLATE:
+"""
