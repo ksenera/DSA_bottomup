bd0be9b1132f137a936c3b2d2eb5d9ac77f4d114
ksenera <ksenera7130@conestogac.on.ca>
2026-03-29 23:35:28 -0400

    graphs: LC323 | POE

--- Practice/graphs.py
+++ Practice/graphs.py
@@ -9,8 +9,8 @@ Output: 2
 Input: n = 5, edges = [[0,1],[1,2],[2,3],[3,4]]
 Output: 1
 
-PREDICTION:
-SIGNAL:
+PREDICTION: undirected graph
+SIGNAL: gives 5 points and 3 edges where each edge must have 2 of the 5 points some points can have 2 edges ?
 FAMILY:
 CATEGORY:
 LOCAL:
