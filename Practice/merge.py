751d0133a8507203cd6af3e00fc8281623114638
ksenera <ksenera7130@conestogac.on.ca>
2026-03-30 23:43:12 -0400

    new class derivation

---
+++ Practice/merge.py
@@ -0,0 +1,9 @@
+"""
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: [1,3] and [2,6] overlap → merge to [1,6]
+
+Input: intervals = [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: [1,4] and [4,5] are considered overlapping
+"""
