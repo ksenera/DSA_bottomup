2e0304cd83ed90da2636150ee7275b428ecfcb35
ksenera <ksenera7130@conestogac.on.ca>
2026-03-31 23:05:44 -0400

    comment

--- Practice/merge.py
+++ Practice/merge.py
@@ -11,7 +11,7 @@ Input: intervals = [[1,4],[4,5]]
 Output: [[1,5]]
 Explanation: [1,4] and [4,5] are considered overlapping
 
-PREDICTION:
+PREDICTION: merging from the name 
 SIGNAL:
 FAMILY:
 CATEGORY:
